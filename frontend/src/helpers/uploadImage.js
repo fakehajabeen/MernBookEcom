ad5d7e01567ba2eb8506d08cd31cb153a3d4c9a5
fakehajabeen <136341482+fakehajabeen@users.noreply.github.com>
2025-03-06 02:50:30 +0530

    Update uploadImage.js

--- frontend/src/helpers/uploadImage.js
+++ frontend/src/helpers/uploadImage.js
@@ -1,5 +1,5 @@
 //const url = `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUD_NAME_CLOUDINARY}/image/upload`
-const url = `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUD_NAME_CLOUDINARY}/image/upload`
+const url = `https://api.cloudinary.com/v1_1/${process.env.REACT_CLOUD}/image/upload`
 
 
 const uploadImage  = async(image) => {
