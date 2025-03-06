479989e9064d8409f456c9b9ffde528864798edc
fakehajabeen <136341482+fakehajabeen@users.noreply.github.com>
2025-03-07 00:29:54 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,12 @@ const app= express();
 app.use(cookieParser())
 app.use(express.json());
 
-app.use(cors({ origin: "https://mern-book-ecom-og7b.vercel.app" }));
+app.use(cors({
+    origin: "https://mern-book-ecom-og7b.vercel.app" ,
+    credentials: true, // Replace with the exact URL of your frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  }));
 
 
 require('./config/db');
