cfeeba493fcd489209bfb537a2fc732a00dd1c89
fakehajabeen <136341482+fakehajabeen@users.noreply.github.com>
2025-03-15 21:32:38 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,13 +17,22 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
-connectDB()
-    .then(() => {
-        console.log('Connected to the database');
-    })
-    .catch((err) => {
-        console.error('Error connecting to the database:', err);
-    });
+const MONGO_URI = "mongodb+srv://fakeha26:rahmat26@mern.3z9ik.mongodb.net/?retryWrites=true&w=majority&appName=MERN"; // Replace with your connection string
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected successfully'))
+.catch((err) => console.error('MongoDB connection error:', err));
+
+// connectDB()
+//     .then(() => {
+//         console.log('Connected to the database');
+//     })
+//     .catch((err) => {
+//         console.error('Error connecting to the database:', err);
+//     });
 
 app.use('/api', router);
 
