28018ebb6b46471732d70efa94d0702cd2b4e755
fakehajabeen <136341482+fakehajabeen@users.noreply.github.com>
2025-03-15 13:36:25 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,16 @@
-const mongoose= require("mongoose");
+const mongoose = require('mongoose');
 
-async function connectDB(){
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('MongoDB Connected');
+  } catch (error) {
+    console.error('MongoDB connection failed:', error.message);
+    process.exit(1);
+  }
+};
 
-    try {
-     await   mongoose.connect(process.env.MONGODB_URI)
-        
-    } catch (error) {
-        console.log(error)
-        
-    }
- }
-
- //mongoose.connect('mongodb://127.0.0.1/MERN-book')
-module.exports= connectDB;
-
+module.exports = connectDB;
